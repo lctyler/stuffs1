8d9103f44505abcf294589678fa1a8048c07554a
lctyler <lctyler>
2013-10-14 12:21:25 -0700

    what I got

--- schedule.c
+++ schedule.c
@@ -5,15 +5,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-
+#include <cstring.h>
+#define MAX_ARG_LENGTH 100
 #define MAX_PROCESSES 10
 #define MAX_ARGUMENTS 10
+
+typedef struct input {
+   char exec[MAX_ARG_LENGTH];
+   char *arg[MAX_ARGUMENTS];
+   int numArgs; 
+
+} input;
 
 void switchHandler(int sig){
    if(sig == SIGCHLD)
       //kill timer in case we got here from an early termination
    //switch to next process
 }
+
+
 
 void alarm (unsigned int seconds) {
    struct itimerval new;
@@ -24,24 +34,45 @@ void alarm (unsigned int seconds) {
    setitimer (ITIMER_REAL, &new, NULL);
 }
 
+void parseInputs(input **inputs, int * num, char **argv, int argc) {
+   int i = 2;
+   inputs[0] = malloc(sizeof(inputs)); 
+   for (i; i < argc; i++) {
+       
+     
+
+
+   }
+  
+   
+
+} 
+
+
+
 int pidTable[MAX_PROCESSES];
-int tableNdx;
+int tableNdx = 0;
 
 
 int main(int argc, char **argv){
    int startTime = 0;
    int ndx;
-   int numProcs;
+   int numProcs = 0;
    int childPid;
+   int *getnNumProcs; 
+   input *inputs[10];
+   
+    
 
-   interpretArgs();
+   
 
    signal(SIGCHLD, switchHandler);
    signal(SIGALRM, switchHandler);   
 
    for (ndx = 0; ndx < numProcs; ndx++){
       if ( childPid = fork() ) {
-         //add childPid to processTable
+         pidTable[tableNdx++] = childPid;
+	 numProcs++;
       }
       else {
          pause();
